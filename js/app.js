4839c03505097f10b8b1e4e755e986527294b73b
dcmotor <zxcvbnm@1234>
2017-03-20 17:52:49 +0530

    e
    e

--- js/app.js
+++ js/app.js
@@ -11,27 +11,12 @@
 	var motoren = document.getElementById('motoren');
 	var motorspd = document.getElementById('motorspd');
 
-
-	pubnub.subscribe({
-		channel: settings.channel,
-		callback: function(m) {
-			if(m.temperature) {
-				document.querySelector('[data-temperature]').dataset.temperature = m.temperature;
-			}
-			if(m.humidity) {
-				document.querySelector('[data-humidity]').dataset.humidity = m.humidity;
-			}
-		}
-	})
-
 	function publishUpdate(data) {
 		pubnub.publish({
 			channel: settings.channel, 
 			message: data
 		});
 	}
-
-	// UI EVENTS
 
 	motoren.addEventListener('change', function(e){
 		publishUpdate({item: 'motoren', open: this.checked});
