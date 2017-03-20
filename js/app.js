53206068da4f87c7f778e21bb4f7f7b4470accf0
dcmotor <zxcvbnm@1234>
2017-03-20 18:16:58 +0530

    g
    g

--- js/app.js
+++ js/app.js
@@ -8,8 +8,37 @@
 
 	var pubnub = PUBNUB(settings);
 
-	var motoren = document.getElementById('motoren');
-	var motorspd = document.getElementById('motorspd');
+	var door = document.getElementById('door');
+	var lightLiving = document.getElementById('lightLiving');
+	var lightPorch = document.getElementById('lightPorch');
+	var fireplace = document.getElementById('fireplace');
+
+	pubnub.subscribe({
+		channel: settings.channel,
+		callback: function(m) {
+			if(m.temperature) {
+				document.querySelector('[data-temperature]').dataset.temperature = m.temperature;
+			}
+			if(m.humidity) {
+				document.querySelector('[data-humidity]').dataset.humidity = m.humidity;
+			}
+		}
+	})
+
+	/* 
+		Data settings:
+
+		Servo
+
+		item: 'door'
+		open: true | false
+
+		LED
+
+		item: 'light-*'
+		brightness: 0 - 10
+
+	*/
 
 	function publishUpdate(data) {
 		pubnub.publish({
@@ -18,12 +47,21 @@
 		});
 	}
 
-	motoren.addEventListener('change', function(e){
-		publishUpdate({item: 'motoren', open: this.checked});
+	// UI EVENTS
+
+	door.addEventListener('change', function(e){
+		publishUpdate({item: 'door', open: this.checked});
 	}, false);
 
-	motorspd.addEventListener('change', function(e){
+	lightLiving.addEventListener('change', function(e){
 		publishUpdate({item: 'light-living', brightness: +this.value});
 	}, false);
 
+	lightPorch.addEventListener('change', function(e){
+		publishUpdate({item: 'light-porch', brightness: +this.value});
+	}, false);
+
+	fireplace.addEventListener('change', function(e){
+		publishUpdate({item: 'fireplace', brightness: +this.value});
+	}, false);
 })();
